5ff81f51e4512f6ee33c301df589ca76af4a02d8
ReyhanGTZ <79628746+ReyhanGTZ@users.noreply.github.com>
2021-04-15 20:12:15 +0700

    update

---
+++ about.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!--Bootstrap core-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/aboutstyle.css">    
+    
+</head>
+<style>
+
+    .wrapper {
+        background: #232323;
+        background-size: cover;
+        height: 89.1vh;
+    }
+    .navbar {
+        background-color: black;
+        }
+
+    .nav-link {
+        text-decoration: none;
+        color: white !important;
+        padding: 6px 13px;
+        font-size: 20px;
+        transition: .4s;
+    }
+
+    .navbar ul li a:hover {
+        background: grey;
+        border-radius: 2px;
+    }
+    
+</style>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">
+            <img width="60" height="60" src="/img/HE tranparent.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon bg-light"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto" id="navbarNav">
+                <li class="nav-item"><a class="nav-link " href="home.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link " href="#">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="https://discord.gg/93cuRvu25k">Discord</a></li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
+        <div class="panel">
+            <div class="card" style="width: 25rem; max-width: 100%; height: auto;">
+                <img class="img-fluid" src="img/HEv3.png" alt="Responsive image">
+                
+              </div>
+            <h1 class="display-1">About</h1>
+            <h2 class="display-2 font-weight-bold">HanExecuterV3</h2>
+            <p class="lead">HanExecuterV3 is an Executor that use multiple Injectors that allows you to executer Scripts in ROBLOX!</p>
+            <hr class="my-2">
+            <p>Injector that HanExecuterV3 use.</p>
+            <p class="lead">
+              <a class="btn btn-dark btn-md" href="https://wearedevs.net/exploits" role="button">WeAreDevs API</a>
+              <a class="btn btn-danger btn-md" href="https://easyexploits.com" role="button">EasyExploit API</a>
+              <a class="btn btn-primary btn-md" href="#" role="button">RobloxHacks API</a>
+            </p>
+          </div>
+          
+    </div>
+    
+</body>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</html>
